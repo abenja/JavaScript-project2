60193bc6c2b8c862a9e0f37dd4618bd7ae6fb498
abenja <73184325+abenja@users.noreply.github.com>
2022-03-30 19:07:26 +0300

    requests

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// functions
+var jsonObj;
+
+function loadData() {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Drake&api_key=32d0ed841fc10576e713bdf8c08166aa&format=json", true)
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("content").innerHTML = xmlhttp.responseText;
+            jsonObj = JSON.parse(xmlhttp.responseText);
+        }
+    }
+
+}
