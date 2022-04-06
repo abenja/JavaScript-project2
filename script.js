54c0079a47ee524d2008090cab7312c037235e67
abenja <73184325+abenja@users.noreply.github.com>
2022-04-06 14:33:35 +0300

    album data fetching

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 var jsonObj;
 var artist;
 var url;
+var albums;
+let albumData;
+
+
+//loads album data with artist.getTopAlbums -method
+function loadAlbums() {
+    url = "https://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums&artist=" + artist + "&limit=10&api_key=32d0ed841fc10576e713bdf8c08166aa&format=json";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            albums = JSON.parse(xmlhttp.responseText);
+            console.log(albums);
+            albumData = albums.topalbums.album[0].name;
+            console.log(albumData);
+        }
+    }
+}
+
 
 // Adds an eventListener to the search box and adds makes the search button clicked
 document.getElementById("inputField").addEventListener("keydown", function (event) {
@@ -29,13 +49,14 @@ function loadFromList() {
     artist = document.getElementById("artistList").value;
     url = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=32d0ed841fc10576e713bdf8c08166aa&format=json";
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", url, true)
+    xmlhttp.open("GET", url, true);
     xmlhttp.send();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             document.getElementById("content").innerHTML = xmlhttp.responseText;
             jsonObj = JSON.parse(xmlhttp.responseText);
             printData(jsonObj);
+            loadAlbums();
         }
     }
 
@@ -52,7 +73,7 @@ function loadFromSearch() {
     } else {
         url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=32d0ed841fc10576e713bdf8c08166aa&format=json";
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", url, true)
+        xmlhttp.open("GET", url, true);
         xmlhttp.send();
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -66,45 +87,35 @@ function loadFromSearch() {
                     document.getElementById("content").innerHTML = xmlhttp.responseText;
                     jsonObj = JSON.parse(xmlhttp.responseText);
                     printData(jsonObj);
+                    loadAlbums();
                     document.getElementById('inputField').style.backgroundColor = "#ffffff";
 
                 }
             }
-            url = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=32d0ed841fc10576e713bdf8c08166aa&format=json";
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.open("GET", url, true)
-            xmlhttp.send();
-            xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    document.getElementById("content").innerHTML = xmlhttp.responseText;
-                    jsonObj = JSON.parse(xmlhttp.responseText);
-                    printData(jsonObj);
-                }
-            }
 
         }
-
-        // Prints the retrieved data into a table
-        function printData() {
-            var data = jsonObj;
-            // Link to the image page of the artist
-            var picUrl = data.artist.url + "/+images";
-
-            var out = "<table>";
-            out += '<tr>';
-            out += '<td>' + 'Artist: ' + data.artist.name + '</td>';
-            out += '<td id="bio">' + 'Bio: ' + data.artist.bio.summary + '</td>';
-            out += '<td><img src="' + data.artist.image[2]['#text'] + '"></td>';
-            out += '<td> <a id="pictureLink" href=' + picUrl + ' target="_blank" rel="noopener noreferrer" >Click here for artist pictures</a>';
-            out += '</tr>';
-            out += '<tr>';
-            out += '<td>' + 'Artist: ' + data.artist.name + '</td>';
-            out += '</tr>';
-            out += "</table>";
-            document.getElementById('content').innerHTML = out;
-            console.log(data);
-
-        }
-        return false;
     }
+}
+
+
+
+
+// Prints the retrieved data into a table
+function printData() {
+    var data = jsonObj;
+    // Link to the image page of the artist
+    var picUrl = data.artist.url + "/+images";
+
+    var out = "<table id='infoTable'>";
+    out += '<tr>';
+    out += '<td>' + 'Artist: ' + data.artist.name + '</td>';
+    out += '<td id="bio">' + 'Bio: ' + data.artist.bio.summary + '</td>';
+    out += '<td><img src="' + data.artist.image[2]['#text'] + '"></td>';
+    out += '<td> <a id="pictureLink" href=' + picUrl + ' target="_blank" rel="noopener noreferrer" >Click here for artist pictures</a>';
+    out += '</tr>';
+    out += '</tr>';
+    out += "</table>";
+    document.getElementById('content').innerHTML = out;
+    console.log(data);
+    return false;
 }
